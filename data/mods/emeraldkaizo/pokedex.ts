61a20c8e24d1f95cde55f6ec166500a1a16eb6c0
Patrick Wolfe <patrick.wolfe@tufts.edu>
2020-11-24 11:01:59 -0500

    Being coding EK Dex

--- data/mods/emeraldkaizo/pokedex.ts
+++ data/mods/emeraldkaizo/pokedex.ts
@@ -1,6 +1,58 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	alakazam: {
+		inherit: true,
+		baseStats: {hp:55, atk: 50, def: 45, spa: 135, spd: 95, spe: 120},
+	},
+	ampharos: {
+		inherit: true,
+		baseStats: {hp:90, atk: 75, def: 85, spa:115, spd: 90, spe: 55},
+	}, 
+	arbok: {
+		inherit: true,
+		baseStats: {hp:60, atk: 95, def: 69, spa: 65, spd: 79, spe: 80},
+	},
+	ariados: {
+		inherit: true,
+		baseStats: {hp:70, atk: 90, def: 70, spa: 60, spd: 70, spe: 40},
+	},
+	articuno: {
+		inherit: true,
+		baseStats: {hp:90, atk: 85, def: 100, spa: 115, spd: 125, spe: 85},
+	},
+	azumarill: {
+		inherit: true,
+		baseStats: {hp:100, atk: 60, def: 80, spa: 60, spd: 80, spe: 50},
+	},
+	beautifly: {
+		inherit: true,
+		baseStats: {hp:60, atk: 70, def: 50, spa: 100, spd: 50, spe: 65},
+	},
+	beedrill: {
+		inherit: true,
+		baseStats: {hp:65, atk: 90, def: 40, spa: 45, spd: 80, spe: 75},
+	},
+	bellossom: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 80, def: 95, spa: 90, spd: 100, spe: 50},
+	},
+	butterfree: {
+		inherit: true,
+		baseStats: {hp: 60, atk: 45, def: 50, spa: 90, spd: 80, spe: 70},
+	},
+	castform: {
+		inherit: true,
+		baseStats: {hp: 75, atk: 110, def: 75, spa: 110, spd: 75, spe: 100},
+	},
+	chansey: {
+		inherit: true,
+		baseStats: {hp: 250, atk: 5, def: 25, spa: 35, spd: 105, spe: 50},
+	},
 	chimecho: {
 		inherit: true,
 		baseStats: {hp:95, atk: 50, def: 80, spa: 95, spd: 90, spe: 65},
-	}
+	},
+	clefable: {
+		inherit: true,
+		baseStats: {hp:95, atk: 70, def: 73, spa: 95, spd: 90, spe: 60},
+	},
 };
